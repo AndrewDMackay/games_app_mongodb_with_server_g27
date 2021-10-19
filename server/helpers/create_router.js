f51eded4543a976708138cb61c8e71cbad6f0cd7
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-19 13:48:54 +0100

    10th commit main

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -81,6 +81,14 @@ const createRouter = function (collection) {
       {_id: ObjectID(id)},
       {$set: updatedData}
     )
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.error(err)
+      res.status(500)
+      res.json({status: 500, error: err})
+    }) 
   })
 
 
