c96a1dc07e762827fb8c6401b19823b301729d1c
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-19 11:56:59 +0100

    4th commit main

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,9 +1,37 @@
 
 const express = require('express');
+const ObjectID = require('mongodb').ObjectID
 
 const createRouter = function (collection) {
 
   const router = express.Router();
+
+
+  // INDEX, get all games from MongoDB, and serve as JSON..
+
+  router.get('/', (req, res) => {
+    collection.find().toArray()
+    .then((docs) => {res.json(docs)})
+    .catch((err) => {
+      console.error(err)
+      res.status(500)
+      res.json({status: 500, error: err})
+    })
+  })
+
+  // SHOW, get one game back via its ID, and serve as JSON..
+
+  router.get('/:id', (req, res) => {
+    const id = req.params.id
+    collection.findOne({_id: ObjectID(id)})
+    .then(() => {res.json(doc)})
+    .catch((err) => {
+      console.error(err)
+      res.status(500)
+      res.json({status: 500, error: err})
+    })  
+  })
+
 
   return router;
 
